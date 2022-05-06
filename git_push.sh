9cd4689e14cbb48934d559e7598ecf37b87dbcf2
xieziyin <xieziyin@bytedance.com>
2022-05-06 14:08:43 +0800

    xzy update

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 #
-# Usage example: /bin/sh ./git_push.sh wing328 swagger-petstore-perl "minor update"
+# Usage example: /bin/sh ./git_push.sh megaxzy swagger-go-client "xzy update"
 
 git_user_id=$1
 git_repo_id=$2
@@ -28,7 +28,7 @@ git init
 # Adds the files in the local repository and stages them for commit.
 git add .
 
-# Commits the tracked changes and prepares them to be pushed to a remote repository. 
+# Commits the tracked changes and prepares them to be pushed to a remote repository.
 git commit -m "$release_note"
 
 # Sets the new remote
@@ -38,8 +38,10 @@ if [ "$git_remote" = "" ]; then # git remote not defined
     if [ "$GIT_TOKEN" = "" ]; then
         echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
         git remote add origin https://github.com/${git_user_id}/${git_repo_id}.git
+        #git remote add origin https://github.com/megaxzy/swagger-go-client.git
     else
         git remote add origin https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git
+         #git remote add origin https://github.com/megaxzy/swagger-go-client.git
     fi
 
 fi
@@ -48,5 +50,10 @@ git pull origin master
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://github.com/${git_user_id}/${git_repo_id}.git"
-git push origin master 2>&1 | grep -v 'To https'
+
+git push origin master #2>&1 | grep -v 'To https'
+expect "Username*"
+send  919482827@qq.com
+expect "Password*"
+send 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
 
